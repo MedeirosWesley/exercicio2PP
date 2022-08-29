cd27e768e160dca75b3c559b1b6b916ddc203bd9
Wesley Medeiros da Cruz <wesleymcruz03@gmail.com>
2022-08-29 10:08:01 -0300

    projeto criado

---
+++ src/Prime.java
@@ -0,0 +1,26 @@
+
+    public class Prime extends Thread {
+        private int n;
+        private int t;
+        public Prime(int n, int t){
+            this.n= n;this.t= t;
+        }
+        public void run(){
+            boolean prime = true;
+            int end = n + 1000;
+            int i = 2;
+            while (n <= end){
+                for(;i<= n/2; i++){
+//                System.out.println(n + "%" + i + "=" + (n % i));
+                    if(n % i == 0){
+                        prime = false;
+                    }
+                }
+                if(prime) System.out.println(n + " thread -> " + t);
+                prime = true;
+                i=2;
+                n++;
+            }
+        }
+    }
+
